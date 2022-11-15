61dbedc97abe1f131fcb48b140ff05e2d842a0c6
JCerme <jorgealfaro196@gmail.com>
2022-11-15 01:52:11 +0100

    redesign cursor

--- js/cursor.js
+++ js/cursor.js
@@ -1,17 +1,14 @@
 const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 15)+"px; left: "+(e.pageX - 15)+"px;")
+    cursor.setAttribute("style", "top: " + (e.pageY-15) + "px; left: " + (e.pageX-15) + "px;")
 });
-
 var longitud = document.getElementsByTagName("a").length;
-for(i=0; i<longitud;i++){
+for (i = 0; i < longitud; i++) {
     var enlace = document.getElementsByTagName("a")[i];
-    enlace.addEventListener('mouseover', () => {
-        cursor.classList.remove("cursor");
+    enlace.addEventListener('mouseover', e => {
         cursor.classList.add("link");
     });
-    enlace.addEventListener('mouseout', () => {
+    enlace.addEventListener('mouseout', e => {
         cursor.classList.remove("link");
-        cursor.classList.add("cursor");
     });
 }
